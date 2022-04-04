8500d8a6a870020880a66018a0995de23953013f
SabinaSW19 <dsie2714@scs.ubbcluj.ro>
2022-04-04 15:25:51 +0300

    added lab3 tests

--- MaxPointsParticipants/src/test/java/service/ServiceTestAddAssignment.java
+++ MaxPointsParticipants/src/test/java/service/ServiceTestAddAssignment.java
@@ -20,7 +20,6 @@ public class ServiceTestAddAssignment {
 
         Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         service.deleteTema("10");
-        Tema tema=new Tema("10","aaa",12,11);
         try {
             service.saveTema("10","aaa",12,11);
             assert (true);
@@ -39,9 +38,147 @@ public class ServiceTestAddAssignment {
         NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
 
         Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
-        Tema tema=new Tema("10","aaa",12,11);
         try {
             service.saveTema(null,"aaa",12,11);
+            assert (false);
+        } catch (ValidationException e) {
+            assert (true);
+        }
+
+    }
+
+    @Test
+    void test_idEmpty() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        try {
+            service.saveTema(null,"aaa",12,11);
+            assert (false);
+        } catch (ValidationException e) {
+            assert (true);
+        }
+
+    }
+
+    @Test
+    void test_descNull() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("10");
+        try {
+            service.saveTema("10",null,12,11);
+            assert (false);
+        } catch (ValidationException e) {
+            assert (true);
+        }
+
+    }
+
+    @Test
+    void test_descEmpty() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("10");
+        try {
+            service.saveTema("10","",12,11);
+            assert (false);
+        } catch (ValidationException e) {
+            assert (true);
+        }
+
+    }
+
+    @Test
+    void test_deadlineNegative() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("10");
+        try {
+            service.saveTema("10","aaa",-1,11);
+            assert (false);
+        } catch (ValidationException e) {
+            assert (true);
+        }
+
+    }
+
+    @Test
+    void test_deadlineOver14() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("10");
+        try {
+            service.saveTema("10","aaa",15,11);
+            assert (false);
+        } catch (ValidationException e) {
+            assert (true);
+        }
+
+    }
+
+    @Test
+    void test_startlineNegative() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("10");
+        try {
+            service.saveTema("10","aaa",11,-1);
+            assert (false);
+        } catch (ValidationException e) {
+            assert (true);
+        }
+
+    }
+
+    @Test
+    void test_startlineOver14() {
+        Validator<Student> studentValidator = new StudentValidator();
+        Validator<Tema> temaValidator = new TemaValidator();
+        Validator<Nota> notaValidator = new NotaValidator();
+        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+
+        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service.deleteTema("10");
+        try {
+            service.saveTema("10","aaa",11,15);
             assert (false);
         } catch (ValidationException e) {
             assert (true);
