c7d9b38d2d38d00e0a79ec743a8ad52cbbe3de6f
SabinaSW19 <dsie2714@scs.ubbcluj.ro>
2022-04-15 08:19:07 +0300

    few modifications

--- MaxPointsParticipants/src/test/java/service/ServiceTestAddGrade.java
+++ MaxPointsParticipants/src/test/java/service/ServiceTestAddGrade.java
@@ -5,6 +5,7 @@ import domain.Student;
 import domain.Tema;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
@@ -12,9 +13,11 @@ import repository.TemaXMLRepository;
 import validation.*;
 
 public class ServiceTestAddGrade {
+    Service service;
 
-    @Test
-    public void test_addStudent() {
+    @BeforeEach
+    public void init()
+    {
         Validator<Student> studentValidator = new StudentValidator();
         Validator<Tema> temaValidator = new TemaValidator();
         Validator<Nota> notaValidator = new NotaValidator();
@@ -22,7 +25,12 @@ public class ServiceTestAddGrade {
         TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
         NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
 
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    @Test
+    public void test_addStudent() {
+
         Student student = new Student("1", "aa", 222, "abcd@yahoo.com", "sdsds");
         service.deleteStudent("100");
         try {
@@ -35,14 +43,7 @@ public class ServiceTestAddGrade {
     }
     @Test
     public void test_addAssignment() {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
 
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         service.deleteTema("100");
         try {
             service.saveTema("100","aaa",12,11);
@@ -54,14 +55,7 @@ public class ServiceTestAddGrade {
     }
     @Test
     public void test_addGrade() {
-        Validator<Student> studentValidator = new StudentValidator();
-        Validator<Tema> temaValidator = new TemaValidator();
-        Validator<Nota> notaValidator = new NotaValidator();
-        StudentXMLRepository fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
-        TemaXMLRepository fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
-        NotaXMLRepository fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
 
-        Service service = new Service(fileRepository1, fileRepository2, fileRepository3);
         service.deleteNota("100","100");
 
         try {
@@ -75,8 +69,30 @@ public class ServiceTestAddGrade {
     @Test
     public void test_BigBang()
     {
-        test_addStudent();
-        test_addAssignment();
-        test_addGrade();
+        service.deleteStudent("100");
+        try {
+            service.saveStudent("100", "aa", 222, "abcd@yahoo.com", "sdsds");
+            assert (true);
+        } catch (ValidationException e) {
+            assert (false);
+        }
+        service.deleteTema("100");
+        try {
+            service.saveTema("100","aaa",12,11);
+            assert (true);
+        } catch (ValidationException e) {
+            assert (false);
+        }
+        service.deleteNota("100","100");
+
+        try {
+            service.saveNota("100","100",10,12,"fantastic");
+            assert (true);
+        } catch (ValidationException e) {
+            assert (false);
+        }
+//        test_addStudent();
+//        test_addAssignment();
+//        test_addGrade();
     }
 }
